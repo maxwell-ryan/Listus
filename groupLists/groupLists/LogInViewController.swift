302d7e37af097481023c1a3c38354ffd7d6553d5
crosskyle <kylcross@gmail.com>
2017-10-10 17:48:38 -0700

    Added views for welcome, login, and register

---
+++ groupLists/groupLists/LogInViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LogInViewController.swift
+//  groupLists
+//
+//  Created by Kyle Cross on 10/10/17.
+//  Copyright © 2017 bergerMacPro. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
