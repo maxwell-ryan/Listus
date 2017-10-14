c8cb29ddc4747041ab065dcbbf74925fba453357
Maxwell Ryan <weinbema@oregonstate.edu>
2017-10-13 19:08:23 -0600

    Added ViewController to manage display and selection of multiple events

---
+++ groupLists/groupLists/EventCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EventCollectionViewCell.swift
+//  groupLists
+//
+//  Created by bergerMacPro on 10/13/17.
+//  Copyright © 2017 bergerMacPro. All rights reserved.
+//
+
+import UIKit
+
+class EventCollectionViewCell: UICollectionViewCell {
+    
+}
