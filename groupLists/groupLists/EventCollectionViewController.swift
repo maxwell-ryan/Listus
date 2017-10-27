036d69d0ae69c8c4a31a23011846364b73a97cac
crosskyle <kylcross@gmail.com>
2017-10-27 15:08:03 -0700

    Comment out getDBEvents due to reverse interval

--- groupLists/groupLists/EventCollectionViewController.swift
+++ groupLists/groupLists/EventCollectionViewController.swift
@@ -53,9 +53,9 @@ class EventCollectionViewController: UIViewController, UICollectionViewDelegate,
         menuBtn.addTarget(self, action: #selector(displayMenu), for: .touchUpInside)
         
         //Create event, move to add event action
-        //userEventsController.createEvent(name: "testEvent1", description: "some description", date: Date.init(timeIntervalSinceNow: 86400.0), userController: userController, eventCollectionView: eventCollectionView)
+        userEventsController.createEvent(name: "testEvent1", description: "some description", date: Date.init(timeIntervalSinceNow: 86400.0 * 60), userController: userController, eventCollectionView: eventCollectionView)
         
-        userEventsController.getDBEvents(userId: userController.user.id, eventCollectionView: self.eventCollectionView)
+        //userEventsController.getDBEvents(userId: userController.user.id, eventCollectionView: self.eventCollectionView)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -91,7 +91,7 @@ class EventCollectionViewController: UIViewController, UICollectionViewDelegate,
         if (indexPath.item % 2 == 0) {
             cell.backgroundColor = colors.primaryColor1
         } else {
-            cell.backgroundColor = colors.primaryColor1 //UIColor.white
+            cell.backgroundColor = colors.primaryColor1
         }
         
         //populate custom cell with event information
